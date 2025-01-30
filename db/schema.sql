f22688887ef1b73024ec3b93776f9bb6ce8ed40f
jake ringate <jakeringate3@gmail.com>
2025-01-30 15:02:48 -0600

    added package.json and tsconfig files took way to long

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,7 @@
-add conection to database
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
 
+\c employee_tracker_db;
 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,18 @@
 -- Insert different departments into deparments table
-INSERT INTO department (name) VALUES ('Engineering');
-INSERT INTO department (name) VALUES ('Human Resources');
-INSERT INTO department (name) VALUES ('Marketing');
+INSERT INTO department (name)
+VALUES ('Engineering'),
+('Human Resources'),
+('Marketing');
 
 -- Insert various roles into role table
-INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', 80000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ('HR Manager', 70000, 2);
-INSERT INTO role (title, salary, department_id) VALUES ('Marketing Specialist', 60000, 3);
+INSERT INTO role (title, salary, department_id) 
+VALUES ('Software Engineer', 80000, 1),
+('HR Manager', 70000, 2),
+('Marketing Specialist', 60000, 3);
 
 -- Insert employee values into employee table
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Alice', 'Johnson', 1, NULL);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Bob', 'Smith', 2, NULL);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Charlie', 'Brown', 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES 
+('Alice', 'Johnson', 1, NULL),
+('Bob', 'Smith', 2, NULL),
+('Charlie', 'Brown', 3, 1);
